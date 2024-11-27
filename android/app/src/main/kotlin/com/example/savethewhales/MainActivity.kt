116d3f3fd9b794b4adf01e726d26db26a5a7436c
AstericsDS <andhika2312@gmail.com>
2024-11-27 10:49:27 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/savethewhales/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.savethewhales
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  const homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Save the Whales',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue[300],
+      ),
+    );
+  }
+}
