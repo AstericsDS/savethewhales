a51839bd15abf24f6d3e3871745edad818f69169
AstericsDS <andhika2312@gmail.com>
2024-11-27 19:59:36 +0700

    added menu and searchfield

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,23 +1,220 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class homepage extends StatelessWidget {
   const homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double boxSize = screenWidth * 0.3;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Save the Whales',
-          style: TextStyle(
+      appBar: theAppBar(),
+      body: Column(
+        children: [
+          searchField(),
+          SizedBox(
+            height: 40,
+          ),
+          mainMenu(boxSize),
+        ],
+      ),
+    );
+  }
+
+  Column mainMenu(double boxSize) {
+    return Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: boxSize,
+                  height: boxSize,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/svgs/whale-1.svg',
+                        width: 75,
+                        height: 75,
+                      ),
+                      Text(
+                        'Option 1',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: boxSize,
+                  height: boxSize,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/svgs/whale-2.svg',
+                        width: 75,
+                        height: 75,
+                      ),
+                      Text(
+                        'Option 2',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: boxSize,
+                  height: boxSize,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/svgs/whale-3.svg',
+                        width: 75,
+                        height: 75,
+                      ),
+                      Text(
+                        'Option 3',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: boxSize,
+                  height: boxSize,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/svgs/whale-4.svg',
+                        width: 75,
+                        height: 75,
+                      ),
+                      Text(
+                        'Option 4',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        );
+  }
+
+  Container searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 40,
+              spreadRadius: 0.0),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            hintText: 'Search anything',
+            hintStyle: TextStyle(
+              color: Colors.black87,
+            ),
+            fillColor: Colors.blue[100],
+            contentPadding: EdgeInsets.all(10),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SvgPicture.asset('assets/svgs/search.svg'),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            )),
+      ),
+    );
+  }
+
+  AppBar theAppBar() {
+    return AppBar(
+      title: Text(
+        'Save the Whales',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blue[300],
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        child: SvgPicture.asset(
+          'assets/svgs/arrow-left.svg',
+          height: 20,
+          width: 20,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      actions: [
+        Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          child: SvgPicture.asset(
+            'assets/svgs/three-dots.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
             color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+            borderRadius: BorderRadius.circular(10),
           ),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.blue[300],
-      ),
+      ],
     );
   }
 }
