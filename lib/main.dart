7d013b29e9a962aa014c52044a813a837651c284
Acokii <zaffaz1106@gmail.com>
2024-11-27 21:54:58 +0700

    push test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: homepage(),
+      home: const Homepage(),
     );
   }
 }
