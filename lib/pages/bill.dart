950f6d46be8fe4c1588d98377da44668dd958b63
AstericsDS <andhika2312@gmail.com>
2024-12-11 10:15:29 +0700

    added routs

---
+++ lib/pages/bill.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class BillPage extends StatelessWidget {
+  const BillPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            searchField(),
+            SizedBox(
+              height: 40,
+            ),
+            mainMenu(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container mainMenu() {
+    return Container(
+      child: Text('This is a bill page'),
+    );
+  }
+
+  Container searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 40,
+              spreadRadius: 0.0),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            hintText: 'Search anything',
+            hintStyle: TextStyle(
+              color: Colors.black87,
+            ),
+            fillColor: Colors.blue[100],
+            contentPadding: EdgeInsets.all(10),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SvgPicture.asset('assets/svgs/search.svg'),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            )),
+      ),
+    );
+  }
+
+}
