faa5cb1ad6041c00015b851dc715ae8dbeb87f42
Acokii <zaffaz1106@gmail.com>
2024-12-24 10:40:45 +0700

    paustad sudah join

---
+++ lib/models/whale.dart
@@ -0,0 +1,13 @@
+class Whale {
+  final int id;
+  final String whaletype;
+  final String desc;
+  final String image;
+
+  Whale ({
+    required this.id,
+    required this.whaletype,
+    required this.desc,
+    required this.image,
+  });
+}
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,5 +1,6 @@
 import 'package:path/path.dart';
 import 'package:savethewhales/models/bill.dart';
+import 'package:savethewhales/models/whale.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseService {
@@ -37,8 +38,47 @@ class DatabaseService {
           $_emailColumnName TEXT NOT NULL,
           $_nominalColumnName TEXT NOT NULL,
           $_paymentColumnName TEXT NOT NULL
-        )''');
-      },
+        );
+        CREATE TABLE whale (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        whaletype TEXT NOT NULL,
+        desc TEXT NOT NULL,
+        image TEXT NOT NULL,
+        );
+
+        INSERT INTO whale (whaletype, desc, image)
+        VALUE
+        (
+          'Orca', 
+          'Paus orca adalah hewan besar yang hidup di laut. Mereka sering disebut "paus pembunuh", 
+          tapi sebenarnya mereka adalah jenis lumba-lumba terbesar. Orca punya tubuh hitam dengan bercak putih, 
+          dan mereka sangat pintar. Mereka makan ikan, anjing laut, dan kadang hewan laut lain. Orca suka berenang 
+          bersama keluarga mereka yang disebut "pod" dan bekerja sama saat berburu makanan. Mereka juga suka 
+          melompat dan bermain di air!', 
+          '/assets/svgs/orca.svg'
+        ),
+
+        (
+          'Sperm Whale', 
+          'Paus sperma adalah paus besar yang hidup di laut. Mereka punya kepala yang sangat besar 
+          dan bentuk tubuh seperti torpedo. Paus sperma suka menyelam sangat dalam untuk mencari makanan, seperti 
+          cumi-cumi raksasa. Mereka bernapas lewat lubang di atas kepala dan bisa menahan napas lama sekali. 
+          Paus sperma tinggal bersama keluarga mereka dan membuat suara untuk berbicara satu sama lain di dalam air. 
+          Mereka adalah penyelam hebat dan sangat kuat!', 
+          '/assets/svgs/spermwhale.svg'
+        ),
+
+        (
+          'Blue Whale', 
+          'Paus biru adalah hewan terbesar di dunia, bahkan lebih besar dari dinosaurus! 
+          Mereka hidup di laut dan punya tubuh yang panjang serta berwarna biru keabu-abuan. 
+          Paus biru makan hewan kecil yang disebut krill. Mereka menelan banyak air, lalu menyaring makanannya. 
+          Meski besar, paus biru sangat lembut dan suka berenang perlahan. 
+          Mereka bernapas lewat lubang di atas kepala, dan semburannya bisa terlihat dari jauh!',
+          '/assets/svgs/bluewhale.svg'
+        ),
+        ''');
+      }, 
     );
     return database;
   }
@@ -69,6 +109,21 @@ class DatabaseService {
             ))
         .toList();
     return bills;
+  }
+
+  Future<List<Whale>> getWhale() async {
+    final db = await database; // Assume 'database' is your initialized database
+    final data = await db
+        .query('whale'); // _tableName should be the name of your bills table
+    List<Whale> whales = data
+        .map((e) => Whale(
+              id: e['id'] as int,
+              whaletype: e['whaletype'] as String,
+              desc: e['desc'] as String,
+              image: e['image'] as String, // Correct field name
+              ))
+        .toList();
+    return whales;
   }
 }
 
